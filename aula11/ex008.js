c35b9e5409e99b9ff845c41b26c9cedfc6617c57
sabrigs <sabrinagsilva@proton.me>
2026-08-13 10:43:10 -0300

    Add new HTML and JavaScript files for various examples including DOM manipulation, event listeners, and a sum calculator

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+const velocity = 20.5;
+if (velocity > 60) {
+    console.log(`Velocidade: ${velocity}`);
+    console.log(`MULTADO! Você ultrapassou a velocidade permitida.`);
+} else {
+    console.log(`Dirija sempre usando o cinto de segurança!`);    
+}
