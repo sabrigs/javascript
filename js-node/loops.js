658e11ef239411b2375b883becf9246ed9ad26ed
sabrigs <sabrinagsilva@proton.me>
2026-08-13 22:40:03 -0300

    Add loops.js file with examples of for, do while, and while loops

---
+++ js-node/loops.js
@@ -0,0 +1,27 @@
+// FOR
+for (let i = 0; i <= 40; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} é par`);
+    } 
+    else {
+        console.log(`${i} é ímpar`);
+    }
+}
+
+// DO WHILE
+/*
+let x = 50;
+do {
+    console.log(x);
+    x--;
+} while (x > 20);
+*/
+
+// WHILE
+/*
+let x = 50;
+while (x > 20) {
+    console.log(x);
+    x--;
+}
+*/
